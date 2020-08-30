205cfc503b0fa44f29e56744b8060e79f7a8f8fe
madaravali <70214978+madaravali@users.noreply.github.com>
2020-08-30 10:18:13 -0700

    Create bag_problem.py

---
+++ bag_problem.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=[]
+for i in range(n):
+     i=int(input())
+     a.append(i)
+sum=0
+t=0
+for i in range(0,len(a)):
+    sum=sum+a[i]
+    if(sum>=5):
+        sum=a[i]
+    if(sum+a[i]>=5):
+        t=t+1
+print(t)
