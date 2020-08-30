0fbd738f70af78d5a481bfc93a932d01adba5ac3
madaravali <70214978+madaravali@users.noreply.github.com>
2020-08-30 10:21:24 -0700

    Update printing_the_subsets_of_a_list.py

--- printing_the_subsets_of_a_list.py
+++ printing_the_subsets_of_a_list.py
@@ -1,6 +1,10 @@
-a=[1,2,3]
+n=int(input())
+a=[]
+for i in range(n):
+    i=int(input())
+    a.append(i)
 t=[]
-for i in range(0,8):
+for i in range(0,2**(len(a))):
     h=[]
     for j in range(len(a)):
          if((i&(1<<j))==(1<<j)):
