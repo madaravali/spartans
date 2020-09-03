af6c2d65e3ccea2409a0d4d285e04f4655c527b0
madaravali <70214978+madaravali@users.noreply.github.com>
2020-09-03 03:52:47 -0700

    Update finding_the_sum_lies_between_2_indexes.py

--- finding_the_sum_lies_between_2_indexes.py
+++ finding_the_sum_lies_between_2_indexes.py
@@ -1,8 +1,8 @@
 a=int(input())
 d=[int(i) for i in input().split()]
 for i in range(len(d)):
+    sum=d[i]
     for j in range(i+1,len(d)):
-        sum=d[i]
         sum=sum+d[j]
         if(sum==a):
             print(i)
