7010a84726d62516574cc67f294aae7180e25260
madaravali <70214978+madaravali@users.noreply.github.com>
2020-12-12 20:38:33 -0800

    Update counting_no._of_triplets.py

--- counting_no._of_triplets.py
+++ counting_no._of_triplets.py
@@ -7,5 +7,6 @@ for i in range(len(a)):
         if(j+1<len(a)):d=a[j+1]
         sum=a[i]+a[j]
         if(sum==d):count=count+1
-        if(sum<d):i=i+1
+        if(sum>d):i=i+1
+        if(sum<d):d=a[j+2]
 print(count) 
